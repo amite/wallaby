5e91018e3b304b52500afe58715e2a36be2562c7
amite <amit.erandole@gmail.com>
2017-09-13 08:27:14 +0530

    inline styles example

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
-import './paper.css'
+const styles = {
+  paper: {
+    textAlign: 'center',
+    background: '#ffffff',
+    padding: '20px 30px',
+    boxShadow:
+      '0 14px 8px 0 rgba(0, 0, 0, 0.08), 0 9px 16px 4px rgba(0, 0, 0, 0.12)',
+    position: 'relative',
+    top: '40%',
+    transform: 'translateY(-50%)',
+    minHeight: '475px',
+    borderRadius: '5px'
+  }
+}
 
-const Paper = ({ children }) => <div className="paper">{children}</div>
+const Paper = ({ children }) => <div style={styles.paper}>{children}</div>
 
 export default Paper
